7b6452849595446485fd2dffa0901dc55ea7757c
Robert Englund <robertenglund980@gmail.com>
2021-01-09 20:13:27 -0600

    Level 1 + start of Level 2

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -1,4 +1,36 @@
-// from data.js
+// data from data.js
 var tableData = data;
+// data from html
+var tbody = d3.select("tbody");
+var button = d3.select("#filter-btn");
+var inputDatetime = d3.select("#datetime");
+var inputCity = d3.select("#city");
 
-// YOUR CODE HERE!
+// Clear and load sightings table when called
+function loadTable(dataX) {
+// Clear table 
+    tbody.html("");
+// Load table
+dataX.forEach(function(UFOSightings) 
+    {var row = tbody.append("tr");
+     Object.entries(UFOSightings).forEach(function([key, value]) 
+        {var cell = row.append("td");
+         cell.text(value);
+        });
+    });
+    console.log("Table loaded");
+};
+
+// Load sightings table with initial set of data
+loadTable(tableData)
+
+// Reload sightings table based on user input 
+button.on("click", function() {
+// get date entered
+    var newDatetime = inputDatetime.property("value").trim();
+// subset of sightings
+    var dataSubset = tableData.filter(tableData => tableData.datetime === newDatetime);
+// reload sightings table
+    loadTable(dataSubset)
+});
+        
